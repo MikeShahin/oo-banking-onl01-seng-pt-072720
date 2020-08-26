ead815d094653d34a4700bcfbb07f63fdd7d02cc
Mike Shahindoust <gobias101@gmail.com>
2020-08-26 00:22:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
     if valid? 
       sender.balance -= amount
       receiver.balance += amount
-      @status = "complete"
+      self.status = "complete"
     else
       "rejected"
     end
