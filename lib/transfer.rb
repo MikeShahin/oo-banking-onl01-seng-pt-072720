927e669b791e253ebdf7f47a0093f9ddc337aaca
Mike Shahindoust <gobias101@gmail.com>
2020-08-26 00:15:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+  
+  attr_reader :sender, :sendee
+  attr_accessor :amount
+  
+  def initialize(sender, sendee, amount)
+    @sender = sender
+    @sendee = sendee
+    @amount = amount
+  end
 end
