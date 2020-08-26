26757488dd646fcc9543982dbc9287718f6c731f
Mike Shahindoust <gobias101@gmail.com>
2020-08-26 00:31:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,7 @@ class Transfer
     if self.status == "complete" && valid?
       receiver.balance -= amount
       sender.balance += amount
+      self.status = "reversed"
     else
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
